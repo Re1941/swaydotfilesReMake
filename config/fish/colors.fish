52af2f5d1d47817ffed980fd76db0193369854b9
Re <nimalikhov@icloud.com>
2026-02-25 20:11:31 +0500

    add files aJDSABjhd

---
+++ config/fish/colors.fish
@@ -0,0 +1,17 @@
+set -g fish_term24bit 1
+
+set -g fish_color_normal "#b3c7ee"
+set -g fish_color_param "#b3c7ee"
+
+set -g fish_color_command "#b3c7ee" --bold
+
+set -g fish_color_autosuggestion "#44474e"
+
+set -g fish_color_quote "#bfc6d8"
+set -g fish_color_comment "#8e9098"
+set -g fish_color_end "#8e9098"
+set -g fish_color_error "#ffb4ab"
+
+set -g fish_pager_color_prefix "#b3c7ee" --bold
+set -g fish_pager_color_completion "#b3c7ee"
+set -g fish_pager_color_description "#44474e"
---
+++ config/fish/config.fish
@@ -0,0 +1,22 @@
+function fish_prompt -d "Write out the prompt"
+    printf '%s@%s %s%s%s > ' $USER $hostname \
+        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+end
+if test -f ~/.config/fish/colors.fish
+    source ~/.config/fish/colors.fish
+end
+if status is-interactive # Commands to run in interactive sessions can go here
+
+    set fish_greeting
+
+
+
+    alias ls 'eza --icons' 
+    alias ff fastfetch
+    alias b btop
+    alias cc 'doas pacman -Scc --noconfirm && yay -Scc --noconfirm'
+    alias up 'doas pacman -Syu --noconfirm && yay -Sua --noconfirm && cc'
+    alias grep rg
+    alias n nvim
+    fastfetch
+end
