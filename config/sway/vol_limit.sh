e10b785342af81d1427ae77dc32b4ae3fc638d0f
Re <nimalikhov@icloud.com>
2026-08-09 15:48:35 +0000

    Update vol_limit.sh

--- config/sway/vol_limit.sh
+++ config/sway/vol_limit.sh
@@ -6,8 +6,8 @@ CUR_VOL=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -oP '\d+(?=%)' | head -n1)
 if [ "$1" == "up" ]; then
     NEW_VOL=$((CUR_VOL + 5))
 
-    if [ "$NEW_VOL" -gt 60 ]; then
-        NEW_VOL=60
+    if [ "$NEW_VOL" -gt 65 ]; then
+        NEW_VOL=65
     fi
     
 elif [ "$1" == "down" ]; then
@@ -17,9 +17,7 @@ else
     exit 0
 fi
 
-
 pactl set-sink-volume @DEFAULT_SINK@ "${NEW_VOL}%"
-
 
 if [ -p /tmp/wobpipe ]; then
     echo "$NEW_VOL" > /tmp/wobpipe
